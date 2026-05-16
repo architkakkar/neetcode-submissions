5282343ce934c8b1415c994a7fe724a0af05149b
Archit Kakkar <106911307+architkakkar@users.noreply.github.com>
2026-05-16 11:38:09 +0530

    Add: buy-and-sell-crypto - submission-0

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-0.java
@@ -0,0 +1,17 @@
+// Brute Force
+// TC: O(n^2)
+// SC: O(1)
+class Solution {
+    public int maxProfit(int[] prices) {
+        int n = prices.length;
+        int profit = 0;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i+1; j < n; j++) {
+                profit = Math.max(profit, prices[j] - prices[i]);
+            }
+        }
+
+        return profit;
+    }
+}
