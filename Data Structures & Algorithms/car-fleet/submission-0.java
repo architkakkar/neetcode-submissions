f901f34cb07480fcee7d06ea398cbc3256f3b873
Archit Kakkar <106911307+architkakkar@users.noreply.github.com>
2026-05-22 00:20:57 +0530

    Add: car-fleet - submission-0

---
+++ Data Structures & Algorithms/car-fleet/submission-0.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int carFleet(int target, int[] position, int[] speed) {
+        int n = position.length;
+        int[][] cars = new int[n][2];
+        int noOfCarFleets = 0;
+        double prevMax = 0;
+
+        for (int i = 0; i < n; i++) {
+            cars[i][0] = position[i];
+            cars[i][1] = speed[i];
+        }
+
+        Arrays.sort(cars, (a, b) -> b[0] - a[0]);
+
+        for (int i = 0; i < n; i++) {
+            double time = (double)(target - cars[i][0]) / cars[i][1];
+
+            if (prevMax < time) {
+                prevMax = time;
+                noOfCarFleets++;
+            }
+        }
+
+        return noOfCarFleets;
+    }
+}
